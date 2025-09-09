fcc06b8c6b034b33c540ba8884ce100e95822439
Jelle Gemert <jelle.gemert@hva.nl>
2025-09-09 12:53:00 +0200

    imported spaceship asset

--- bird/Assets/Scripts/MovementScript.cs
+++ bird/Assets/Scripts/MovementScript.cs
@@ -13,10 +13,23 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //forward and up
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(transform.forward * push, ForceMode.Impulse);
             rb.AddForce(transform.up * push, ForceMode.Impulse);
+        }
+
+        //right
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.AddForce(transform.right * push, ForceMode.Impulse);
+        }
+
+        //left
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-transform.right * push, ForceMode.Impulse);
         }
     }
 }
